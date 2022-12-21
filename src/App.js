f03a1ae204082233980ff76a776a71ae59b1d12c
rajeshkumar <rajeshkumar@marketsimplified.com>
2022-12-21 23:33:50 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Movie from './movie/Movie';
+
+import './index.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Movie />
+        </div>
+    )
+}
+
+export default App
