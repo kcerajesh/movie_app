408a567288367150a2ada6a08d9d81deb353acc8
rajeshkumar <rajeshkumar@marketsimplified.com>
2022-12-28 23:17:04 +0530

    header menu and  movie details pages modified for all type devices

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,51 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 import { Images } from '../../constants';
 
 import './Header.scss';
 
 const Header = () => {
+  const [Clicked, setClciked] = useState(false);
+  const handleClicked = () => setClciked(!Clicked);
+
   return (
     <div className="header">
       <div className="header__left">
         <Link to="/" className="header__item">
           <img className="header__icon" src={Images.Logo} />
         </Link>
-        <Link to="/movies/popular" className="header__item"><span>Popular</span></Link>
-        <Link to="/movies/top_rated" className="header__item"><span>Top Rated</span></Link>
-        <Link to="/movies/upcoming" className="header__item"><span>Upcoming</span></Link>
+        <div className={Clicked ? "header__links active" : "header__links"}>
+          {['popular', 'top_rated', 'upcoming'].map((item) => (
+            <>
+              <Link to={`/movies/${item}`} className="header__item" onClick={handleClicked}><span>{item.toUpperCase()}</span></Link>
+            </>
+          ))}
+        </div>
       </div>
       <div className="header__right">
-
+        <div className="header__menu" onClick={handleClicked}>
+          {Clicked ? <FaTimes /> : <FaBars />}
+        </div>
       </div>
+      {/* <div className="header__left">
+        <Link to="/" className="header__item">
+          <img className="header__icon" src={Images.Logo} />
+        </Link>
+        <ul className={Clicked ? "header__item" : "header__item"}>
+          {['popular', 'top_rated', 'upcoming'].map((item) => (
+            <li className="app__flex p-text" key={`link-${item}`}>
+              <div />
+              <Link to={`/movies/${item}`} className="header__item"><span>{item.toUpperCase()}</span></Link>
+            </li>
+          ))}
+        </ul>
+        <div className="header-menu" onClick={handleClicked}>
+          {Clicked ? <FaTimes /> : <FaBars />}
+        </div>
+      </div> */}
     </div>
   )
 }
--- src/pages/movie/Movie.jsx
+++ src/pages/movie/Movie.jsx
@@ -34,18 +34,16 @@ const Movie = () => {
         {loading ?
           <Spinner />
           :
-          <div className="movie">
+          <div className="movie__container">
             <div className="movie__intro">
               <img className="movie__backdrop" src={API.getPosterUrl(movieDetail)} />
             </div>
-            <div className="movie__detail">
-              <div className="movie__detail-left">
-                <div className="poster__box">
-                  <img className="movie__poster" src={`https://image.tmdb.org/t/p/original${movieDetail ? movieDetail.poster_path : ""}`} />
+            <div className="movie">
+              <div className="movie__content">
+                <div className="movie__poster">
+                  <img src={`https://image.tmdb.org/t/p/original${movieDetail ? movieDetail.poster_path : ""}`} />
                 </div>
-              </div>
-              <div className="movie__detail-right">
-                <div className="right__top">
+                <div className="movie__detail">
                   <div className="movie__name">{movieDetail ? movieDetail.original_title : ""}</div>
                   <div className="movie-tagline">{movieDetail ? movieDetail.tagline : ""}</div>
                   <div className="movie__rating">
@@ -55,62 +53,57 @@ const Movie = () => {
                   <div className="movie__runtime">{movieDetail ? movieDetail.runtime + " mins" : ""}</div>
                   <div className="movie__releaseDate">{movieDetail ? "Release date: " + movieDetail.release_date : ""}</div>
                   <div className="movie__genres">
-                    {
-                      movieDetail && movieDetail.genres
-                        ?
-                        movieDetail.genres.map(genre => (
-                          <><span className="movie__genre" key={genre.id} id={genre.id}>{genre.name}</span></>
-                        ))
-                        :
-                        ""
-                    }
+                    {movieDetail && movieDetail.genres ?
+                      movieDetail.genres.map(genre => (
+                        <><span className="movie__genre" key={genre.id} id={genre.id}>{genre.name}</span></>
+                      )) : ""}
+                  </div>
+                  <div className='movie__overview'>
+                    <div className="synopsis__text">Synopsis</div>
+                    {movieDetail ? movieDetail.overview : ""}
                   </div>
                 </div>
-                <div className="right__bottom">
-                  <div className="synopsis__text">Synopsis</div>
-                  <div>{movieDetail ? movieDetail.overview : ""}</div>
-                </div>
               </div>
-            </div>
-            <div className="movie__links">
               <div className="movie__heading">Useful Links</div>
-              {
-                movieDetail && movieDetail.homepage &&
-                <a href={movieDetail.homepage} target="_blank" style={{ textDecoration: "none" }}>
-                  <p>
-                    <span className="movie__home-button movie__button">Homepage
-                      <RiEditBoxLine />
-                    </span>
-                  </p>
-                </a>
-              }
-              {
-                movieDetail && movieDetail.imdb_id &&
-                <a href={"https://www.imdb.com/title/" + movieDetail.imdb_id} target="_blank" style={{ textDecoration: "none" }}>
-                  <p>
-                    <span className="movie__imdb-button movie__button">IMDb
-                      <RiEditBoxLine />
-                    </span>
-                  </p>
-                </a>
-              }
-            </div>
-            <div className="movie__heading">Production companies</div>
-            <div className="movie__production">
-              {
-                movieDetail && movieDetail.production_companies && movieDetail.production_companies.map(company => (
-                  <>
-                    {
-                      company.logo_path
-                      &&
-                      <span className="prod__company-image">
-                        <img className="movie__prod-company" src={"https://image.tmdb.org/t/p/original" + company.logo_path} />
-                        <span>{company.name}</span>
+              <div className="movie__links">
+                {
+                  movieDetail && movieDetail.homepage &&
+                  <a href={movieDetail.homepage} target="_blank" style={{ textDecoration: "none" }}>
+                    <p>
+                      <span className="movie__home-button movie__button">Homepage
+                        <RiEditBoxLine />
                       </span>
-                    }
-                  </>
-                ))
-              }
+                    </p>
+                  </a>
+                }
+                {
+                  movieDetail && movieDetail.imdb_id &&
+                  <a href={"https://www.imdb.com/title/" + movieDetail.imdb_id} target="_blank" style={{ textDecoration: "none" }}>
+                    <p>
+                      <span className="movie__imdb-button movie__button">IMDb
+                        <RiEditBoxLine />
+                      </span>
+                    </p>
+                  </a>
+                }
+              </div>
+              <div className="movie__heading">Production companies</div>
+              <div className="movie__production">
+                {
+                  movieDetail && movieDetail.production_companies && movieDetail.production_companies.map(company => (
+                    <div>
+                      {
+                        company.logo_path
+                        &&
+                        <span className="prod__company">
+                          <img src={"https://image.tmdb.org/t/p/original" + company.logo_path} />
+                          <span>{company.name}</span>
+                        </span>
+                      }
+                    </div>
+                  ))
+                }
+              </div>
             </div>
           </div>
         }
